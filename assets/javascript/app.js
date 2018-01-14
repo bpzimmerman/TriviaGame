da88eb614850be4770875ae92043c8b04e401738
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-01-14 11:10:22 -0500

    added methods to choreograph the various pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,123 @@
 $(document).ready(function(){
     var game = {
+        //array position of the question being asked
+        count: 0,
+        //total number of questions to be asked
+        totalQuestions: 3,
+        //time allotted to answer a question
+        questionTime: 10,
+        //time allotted for the answer page to be displayed
+        answerTime: 5,
+        //setInterval handle variable
+        showQuestion: undefined,
+        //array to hold the data object properties
         keyArray: [],
+        //method to populate the keyArray with the data object properties
         getKeys: function(object){
             for (var key in object){
                 this.keyArray.push(key);
             };
         },
-        shuffle: function (array){
+        //method to randomize an array
+        shuffle: function(array){
             var i = 0;
             var j = 0;
             var temp = "";
-    
             for (i = array.length - 1; i > 0; i -= 1){
-            j = Math.floor(Math.random() * i);
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+                j = Math.floor(Math.random() * i);
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            };
+        },
+        //method to start the game
+        begin: function(){
+            //reset the html and variables
+            $(".row").empty();
+            this.count = 0;
+            //populate and randomize the keyArray with the properties from questions
+            this.getKeys(questions);
+            this.shuffle(this.keyArray);
+            //display the first question
+            this.displayQuestion();
+            //set the interval between each subsequent question
+            this.showQuestion = setInterval(function(){
+                game.nextQuestion()
+            }, 1000 * (this.questionTime + this.answerTime));
+        },
+        //method to display each question
+        displayQuestion: function(){
+            //set function specific variables
+            var q = questions[this.keyArray[this.count]].question;
+            var c = questions[this.keyArray[this.count]].choices;
+            var remTime = this.questionTime;
+            //randomize the choices and clear the html
+            this.shuffle(c);
+            $(".row").empty();
+            //populate the html with current question of total questions
+            document.getElementById("questionNum").innerHTML = "<p>Question " + (this.count + 1) + " of " + this.totalQuestions;
+            //display the time left to answer the current question
+            document.getElementById("timer").innerHTML = "<p>Remaining Time: " + remTime + " seconds</p>";
+            //timer to count down the remaining time and when it hits 0 stop the timer and display the answer page
+            var timeId = setInterval(function(){
+                remTime -= 1;
+                document.getElementById("timer").innerHTML = "<p>Remaining Time: " + remTime + " seconds</p>";
+                if (remTime === 0){
+                    clearInterval(timeId);
+                    game.displayAnswer();
+                }
+            }, 1000);
+            //display the current question and the choices
+            document.getElementById("question").innerHTML = "<p>" + q + "</p>";
+            for (var i = 0; i < c.length; i += 1){
+                var para = document.createElement("p");
+                var t = document.createTextNode(c[i]);
+                para.appendChild(t);
+                document.getElementById("choices").appendChild(para);
+            };
+        },
+        //method to display the answer page
+        displayAnswer: function(){
+            //set function specific variables and clear the html
+            var a = questions[this.keyArray[this.count]].answer;
+            var t = questions[this.keyArray[this.count]].trivia;
+            $(".row").empty();
+            //populate the html with the answer result, correct answer, and trivia tidbit
+            document.getElementById("timer").innerHTML = "<p>Correct!</p>";
+            document.getElementById("question").innerHTML = "<p>" + a + "</p>";
+            document.getElementById("choices").innerHTML = "<p>" + t + "</p>";
+            //displays the final game statistics if this is the last question
+            if ((this.count + 1) === this.totalQuestions){
+                setTimeout(function(){
+                    game.gameStats();
+                }, 1000 * this.answerTime);
+            };
+        },
+        //method to increment the count, stop the interval if it's the last question, and display the next question
+        nextQuestion: function(){
+            this.count += 1;
+            if ((this.count + 1) === this.totalQuestions){
+                clearInterval(this.showQuestion);
+            };
+            this.displayQuestion();
+        },
+        //method to display the final game statistics and add a button to start the game over
+        gameStats: function(){
+            $(".row").empty();
+            document.getElementById("timer").innerHTML = "<p>Number Correct: " + 2 + "</p>";
+            document.getElementById("question").innerHTML = "<p>Number Incorrect: " + 1 + "</p>";
+            document.getElementById("choices").innerHTML = "<p>Number Unanswered: " + 1 + "</p>";
+            document.getElementById("button").innerHTML = "<button id = 'start' class = 'btn'>Start Over?</button>";
+            document.getElementById("start").onclick = function(){
+                game.begin();
             };
         }
     };
 
-    game.getKeys(questions);
-    game.shuffle(game.keyArray);
+    //initial on-load page (button to start the game)
+    document.getElementById("button").innerHTML = "<button id = 'start' class = 'btn'>Start</button>";
 
-    console.log(game.keyArray);
-    console.log(questions[game.keyArray[0]]);
-
+    document.getElementById("start").onclick = function(){
+        game.begin();
+    };
 });
--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -2,51 +2,61 @@ var questions = {
     q1:{
         question: "What weather phenomenon causes more deaths in the U.S. annually than any other except lightning?",
         choices: ["Tornados", "Hurricanes", "Snowstorms", "Hail"],
-        answer: "Tornados"
+        answer: "Tornados",
+        trivia: "Most tornados have wind speeds less than 110 miles per hour (180 km/h), are about 250 feet (80 m) across, and travel a few miles (several kilometers) before dissipating. The most extreme tornados can attain wind speeds of more than 300 miles per hour (480 km/h), are more than two miles (3 km) in diameter, and stay on the ground for dozens of miles (more than 100 km). On average, tornados cause 1500 injuries and 80 deaths per year in the United States."
     },
     q2:{
-        question: "What is given to wood whose normal cells have been replaced with mineral deposits?",
+        question: "What name is given to wood whose normal cells have been replaced with mineral deposits?",
         choices: ["Petrified Wood", "Stone Timber", "Rock Lumber", "Gravel Wood"],
-        answer: "Petrified Wood"
+        answer: "Petrified Wood",
+        trivia: "Unlike other types of fossils which are typically impressions or compressions, petrified wood is a three-dimensional representation of the original organic material. Mineral-laden water deposits minerals in the plant's cells and as the plant's lignin and cellulose decay, a stone mold forms in its place."
     },
     q3:{
         question: "What is pollination by birds is called?",
         choices: ["Autogamy", "Ornithophily", "Entomophily", "Anemophily"],
-        answer: "Ornithophily"
+        answer: "Ornithophily",
+        trivia: "Bird pollination is considered as a costly strategy for plants and it evolves only where there are particular benefits for the plant. High altitude ecosystems that lack insect pollinators, those in dry regions or isolated islands tend to favour the evolution of ornithophily in plants."
     },
     q4:{
         question: "Unlike rodents, rabbits have how many incisor teeth?",
         choices: ["Two", "Three", "Four", "Six"],
-        answer: "Four"
+        answer: "Four",
+        trivia: "The teeth of rabbits are similar and yet different from rodents. Both rodents and rabbits have a indeterminate growth in their incisors (they grow constantly). They also have hard enamel on the external surface of the incisors and a soft dentin behind, but the front of rabbit incisors is white and not orange like rodents. Rabbits also have two sets of top incisors, not one."
     },
     q5:{
         question: "What percent of fire-related deaths are due to smoke inhalation rather than burns?",
         choices: ["10%", "50%", "80%", "99%"],
-        answer: "80%"
+        answer: "80%",
+        trivia: "Smoke inhalation is the primary cause of death for victims of indoor fires. The inhalation or exposure to hot gaseous products of combustion can cause serious respiratory complications. The hot smoke injures or kills by a combination of thermal damage, poisoning and pulmonary irritation and swelling, caused by carbon monoxide, cyanide and other combustion products."
     },
     q6:{
         question: "To what familiar fruit is the plantain similar?",
         choices: ["Mangos", "Bananas", "Kiwi", "Papaya"],
-        answer: "Bananas"
+        answer: "Bananas",
+        trivia: "Plantains are closely related to the common banana, but the fruit, which is green, is typically larger than the common banana. The edible fruit of the plantain has more starch than the banana and is not eaten raw. Because plantains have a maximum of starch before they ripen, they are usually cooked green, either boiled or fried, often with coconut juice or sugar as a flavouring. They may also be dried for later use in cooking or ground for use as a meal. The plantain meal can be further refined to a flour."
     },
     q7:{
         question: "What U.S. President was recognized as a world authority on American game animals?",
         choices: ["Theodore Roosevelt", "Thomas Jefferson", "Dwight Eisenhower", "Woodrow Wilson"],
-        answer: "Theodore Roosevelt"
+        answer: "Theodore Roosevelt",
+        trivia: "Of all Roosevelt's achievements, he was proudest of his work in conservation of natural resources, and extending federal protection to land and wildlife. Roosevelt established the United States Forest Service, signed into law the creation of five National Parks, and signed the 1906 Antiquities Act, under which he proclaimed 18 new U.S. National Monuments. He also established the first 51 bird reserves, four game preserves, and 150 National Forests, including Shoshone National Forest, the nation's first. The area of the United States that he placed under public protection totals approximately 230,000,000 acres (930,000 km2)."
     },
     q8:{
         question: "How many men have walked on the moon?",
         choices: ["Eight", "Ten", "Twelve", "Fourteen"],
-        answer: "Twelve"
+        answer: "Twelve",
+        trivia: "Neil Armstrong and Buzz Aldrin from Apollo 11 on July 21,1969. Pete Conrad and Alan Bean from Apollo 12 on November 19 to 20, 1969. Alan Shepard and Edgar Mitchell from Apollo 14 on February 5 to 6, 1971. David Scott and James Irwin from Apollo 15 on July 31 to August 2, 1971. John Young and Chalres Duke from Apollo 16 on April 21 to 23, 1972. Eugene Cernan and Harrison Schmitt from Apollo 17 on December 11 to 14, 1972."
     },
     q9:{
         question: "What is the fastest-running terrestrial animal?",
         choices: ["Cheetah", "Lion", "Dog", "Jaguar"],
-        answer: "Cheetah"
+        answer: "Cheetah",
+        trivia: "The cheetah is the fastest feline and overall fastest land-animal. The cheetah can accelerate from 0 to 60 mph (96.6 km/h) in under three seconds and can reach a top speeds of around 75 mph (120 km/h), though endurance is limited. The second fastest is the Pronghorn, which can reach a top speed of about 61 mph."
     },
     q10:{
-        question: "In what country do the greatest number of tornadoes occur?",
+        question: "In what country do the greatest number of tornados occur?",
         choices: ["United States", "China", "Canada", "Russia"],
-        answer: "United States"
+        answer: "United States",
+        trivia: "Tornados have been observed and documented on every continent except Antarctica; however, the vast majority of tornados occur in the Tornado Alley region of the United States. The United States averages around 1200 tornados per year; Canada has the second most at around 100 per year."
     }
 };
